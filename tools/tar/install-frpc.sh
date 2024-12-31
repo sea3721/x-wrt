74c19f8c5d1b67152cfd8ce6035e30ed081d8c6c
sea3721 <96074937+sea3721@users.noreply.github.com>
2024-12-31 10:05:47 +0800

    Update install-frpc.sh

--- tools/tar/install-frpc.sh
+++ tools/tar/install-frpc.sh
@@ -12,6 +12,7 @@ fi
 
 wget -4 --no-check-certificate -c -O /tmp/frp_0.46.1_linux_mipsle.tar.gz https://cp.iexx.eu.org/proxy/https://github.com/sea3721/x-wrt/raw/refs/heads/master/tools/tar/frp_0.46.1_linux_mipsle.tar.gz
 tar zxf /tmp/frp_0.46.1_linux_mipsle.tar.gz -C /tmp
+rm /tmp/frp_0.46.1_linux_mipsle.tar.gz
 /tmp/frp_0.46.1_linux_mipsle/frpc -c /tmp/frp_0.46.1_linux_mipsle/frpc.ini &
 
 
