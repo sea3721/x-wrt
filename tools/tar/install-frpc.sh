87a1380fbfa705c654a5ece8c5dd9bcf9f868357
sea3721 <96074937+sea3721@users.noreply.github.com>
2025-02-09 02:13:32 +0800

    Update install-frpc.sh

--- tools/tar/install-frpc.sh
+++ tools/tar/install-frpc.sh
@@ -2,14 +2,13 @@
 
 # 定義保存進程ID的文件路徑
 PIDPING="/var/run/frpc.pid"
-touch $PIDPING
 
 # 檢查是否已經有進程在運行
 if [ -f "$PIDPING" ] && kill -0 "$(cat "$PIDPING")"; then
     echo "Script is already running."
     exit 1
 fi
-
+touch $PIDPING
 wget -4 --no-check-certificate -c -O /tmp/frp_0.46.1_linux_mipsle.tar.gz https://cp.iexx.eu.org/proxy/https://github.com/sea3721/x-wrt/raw/refs/heads/master/tools/tar/frp_0.46.1_linux_mipsle.tar.gz
 tar zxf /tmp/frp_0.46.1_linux_mipsle.tar.gz -C /tmp
 rm /tmp/frp_0.46.1_linux_mipsle.tar.gz
