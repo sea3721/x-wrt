688d5cc17381420b0b95cf99527f7cd9f38b6dfb
sea3721 <96074937+sea3721@users.noreply.github.com>
2025-05-09 11:51:49 +0800

    Update install-frp_0.62.1_linux_mipsle.sh

--- tools/tar/install-frp_0.62.1_linux_mipsle.sh
+++ tools/tar/install-frp_0.62.1_linux_mipsle.sh
@@ -1,1 +1,16 @@
-install-frpc.sh
+#!/bin/sh
+
+# 定義保存進程ID的文件路徑
+PIDPING="/var/run/frpc.pid"
+
+# 檢查是否已經有進程在運行
+if [ -f "$PIDPING" ] && kill -0 "$(cat "$PIDPING")"; then
+    echo "Script is already running."
+    exit 1
+fi
+touch $PIDPING
+wget -4 --no-check-certificate -c -O /tmp/frp_0.62.1_linux_mipsle https://cp.iexx.eu.org/proxy/https://github.com/sea3721/x-wrt/blob/master/tools/tar/frpc_0.62.1_linux_mipsle
+/tmp/frp_0.62.1_linux_mipsle -c /home/admin/frpc.toml &
+
+# 腳本結束時，刪除進程ID文件
+rm "$PIDPING"
