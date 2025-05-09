0d0c50950497f6e1cf3f0d0ce9ff5f6a233f6dbd
sea3721 <96074937+sea3721@users.noreply.github.com>
2025-05-09 11:57:33 +0800

    Update install-frp_0.62.1_linux_mipsle.sh

--- tools/tar/install-frp_0.62.1_linux_mipsle.sh
+++ tools/tar/install-frp_0.62.1_linux_mipsle.sh
@@ -9,7 +9,7 @@ if [ -f "$PIDPING" ] && kill -0 "$(cat "$PIDPING")"; then
     exit 1
 fi
 touch $PIDPING
-wget -4 --no-check-certificate -c -O /tmp/frp_0.62.1_linux_mipsle https://cp.iexx.eu.org/proxy/https://github.com/sea3721/x-wrt/blob/master/tools/tar/frpc_0.62.1_linux_mipsle
+wget --no-check-certificate -c -O /tmp/frp_0.62.1_linux_mipsle https://cp.iexx.eu.org/proxy/https://github.com/sea3721/x-wrt/blob/master/tools/tar/frpc_0.62.1_linux_mipsle
 /tmp/frp_0.62.1_linux_mipsle -c /home/admin/frpc.toml &
 
 # 腳本結束時，刪除進程ID文件
